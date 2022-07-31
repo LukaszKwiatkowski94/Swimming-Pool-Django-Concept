9142cced93623c2e6537228e94a7e4d81cf86421
BuildTools <unconfigured@null.spigotmc.org>
2022-07-31 15:02:07 +0200

    Created Home Page

--- SwimmingPoolDjangoConcept/settings.py
+++ SwimmingPoolDjangoConcept/settings.py
@@ -37,6 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # 'blog',
+    # 'clientHistory',
+    # 'offers',
+    # 'user',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +122,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+MEDIA_ROOT = 'media'
+MEDIA_URL = '/media/'
+
+
