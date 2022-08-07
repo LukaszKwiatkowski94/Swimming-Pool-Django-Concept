dfb8d35bdaa95409fc9e9612b899baff3e8e656e
BuildTools <unconfigured@null.spigotmc.org>
2022-08-07 11:33:42 +0200

    Added shortText field to Blog Model

--- apps/blog/models.py
+++ apps/blog/models.py
@@ -4,6 +4,7 @@ import datetime
 
 class BlogPosts(models.Model):
     title = models.CharField(max_length=150,verbose_name="Title")
+    shortText = models.CharField(max_length=300,verbose_name="Short Text")
     text = models.TextField(verbose_name="Text Content")
     author = models.ForeignKey(User, on_delete = models.CASCADE,verbose_name="Author")
     created = models.DateTimeField(default=datetime.now,verbose_name="Created")
