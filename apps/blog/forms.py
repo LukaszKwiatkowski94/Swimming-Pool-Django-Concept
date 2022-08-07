f833e3dcec15ef2d1e57614553821781af2d9d0f
BuildTools <unconfigured@null.spigotmc.org>
2022-08-07 15:53:42 +0200

    Edit label in Post Creation Form

--- apps/blog/forms.py
+++ apps/blog/forms.py
@@ -10,5 +10,6 @@ class PostCreationForm(forms.Form):
             "shortText":"Short Text (max 300 characters):",
             "text":"Full text:",
             "photo":"Photo to post",
+            "photoAlt":"Alt to photo",
             "published":"Should the post be public:"
         }
