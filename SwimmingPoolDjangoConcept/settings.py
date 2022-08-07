5185add758c4262e679903bf91901e32ad5750e4
BuildTools <unconfigured@null.spigotmc.org>
2022-08-07 09:44:41 +0200

    Blog - model data
    Added model for blog data in blog app

--- SwimmingPoolDjangoConcept/settings.py
+++ SwimmingPoolDjangoConcept/settings.py
@@ -38,10 +38,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.blog',
+    # 'apps.clientHistory',
+    # 'apps.offers',
     'apps.user',
-    # 'blog',
-    # 'clientHistory',
-    # 'offers',
 ]
 
 MIDDLEWARE = [
--- apps/blog/models.py
+++ apps/blog/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from apps.user.models import User
+import datetime
 
-# Create your models here.
+class BlogPosts(models.Model):
+    title = models.CharField(max_length=150,verbose_name="Title")
+    text = models.TextField(verbose_name="Text Content")
+    author = models.ForeignKey(User, on_delete = models.CASCADE,verbose_name="Author")
+    created = models.DateTimeField(default=datetime.now,verbose_name="Created")
+    liked = models.IntegerField(default=0,verbose_name="Liked")
+    photo = models.FileField(upload_to='blogPhoto/', verbose_name="Photo")
+    published = models.BooleanField(default=False,verbose_name="Published")
+
