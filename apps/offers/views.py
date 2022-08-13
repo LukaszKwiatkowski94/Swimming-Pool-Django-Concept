14f6039c09a58a602de4d7912b1a73fcfc43142c
BuildTools <unconfigured@null.spigotmc.org>
2022-08-13 21:52:22 +0200

    Add methods to views in offers app

--- apps/offers/views.py
+++ apps/offers/views.py
@@ -1,3 +1,64 @@
+from multiprocessing import context
+from django.http import Http404, HttpResponse
 from django.shortcuts import render
+from .models import Passes
+from .forms import PassForm
 
-# Create your views here.
+def show(request):
+    try:
+        passes = Passes.objects.filter(active=True)
+        context = {
+            'passes':passes
+        }
+        return render(request,'show.html',context)
+    except:
+        raise Http404("Get Passes does not exist")
+
+def createPass(request):
+    try:
+        form = None
+        if request.POST:
+            form = PassForm(request.POST)
+            if form.is_valid():
+                form.save()
+        if request.GET:
+            form = PassForm()
+            context = {
+                'form':form,
+                'name':"create"
+            }
+        return render(request, 'create-update.html',context)
+    except:
+        raise Http404("Create Pass does not exist")
+
+def updatePass(request,idPass):
+    try:
+        form = None
+        instance = Passes.object.get(id=idPass)
+        if request.POST:
+            form = PassForm(request.POST)
+            if form.is_valid():
+                obj = form.save(commit=False)
+                instance.active = False
+                instance.save()
+                newPass = Passes(namePass=obj.namePass,daysOfUse=obj.daysOfUse,price=obj.price)
+                newPass.save()
+        if request.GET:
+            form = PassForm(instance)
+            context = {
+                'form':form,
+                'name':"update"
+            }
+        return render(request, 'create-update.html',context)
+    except:
+        raise Http404("Update Pass does not exist")
+
+def showAllListPasses(request):
+    try:
+        passes = Passes.objects.all().order_by(active=True)
+        context = {
+            'passes':passes
+        }
+        return render(request,'showAllListPasses.html',context)
+    except:
+        raise Http404("Passes does not exist")
