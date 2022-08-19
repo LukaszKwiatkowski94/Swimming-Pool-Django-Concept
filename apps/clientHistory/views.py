292f0ada4992cf0324eff2c4914ab275c72c7e86
BuildTools <unconfigured@null.spigotmc.org>
2022-08-19 18:57:58 +0200

    Add method to view in clientHistory app

--- apps/clientHistory/views.py
+++ apps/clientHistory/views.py
@@ -1,3 +1,41 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+from django.core import serializers
+from django.core.serializers.json import DjangoJSONEncoder
+from .models import ClientHistoryPasses
+from user.models import User
+import json
 
-# Create your views here.
+def showMyHistory(request):
+    try:
+        historyPasses = ClientHistoryPasses.objects.filter()
+        context = {
+            'historyPasses':historyPasses
+        }
+    except:
+        raise Http404("Client History Passes does not exist")
+    render(request,'showHistoryClient.html', context)
+
+def showClientHistory(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        idUser = body['idUser']
+        userObj = User.objects.get(id=idUser)
+        history = ClientHistoryPasses.objects.filter(user=userObj)
+        data = serializers.serialize('json', history)
+        return HttpResponse(data, content_type='application/json')
+    else:
+        raise Http404("Client History Passes does not exist")
+
+def createNewPassRecord(request):
+    if request.method == 'POST':
+        body_unicode = request.body.decode('utf-8')
+        body = json.loads(body_unicode)
+        idUser = body['idUser']
+        idPass = body['passId']
+        dateEndPost = body['dateEnd']
+        newRecord = ClientHistoryPasses(user=idUser,passID=idPass,dateEnd=dateEndPost)
+        newRecord.save()
+    else:
+        raise Http404("Client History Passes does not exist")
