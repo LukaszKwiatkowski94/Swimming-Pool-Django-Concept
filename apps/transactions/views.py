e9ebb240e3e483d8919dca1934248c80520d6ea0
BuildTools <unconfigured@null.spigotmc.org>
2022-08-25 14:57:04 +0200

    Transaction view

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,16 +1,20 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
 from apps.user.models import User, Wallet
 from apps.offers.models import Passes
 
 def showPageTransaction(request,idPass):
-    wallet = Wallet.objects.get(user = request.user)
-    passBuy = Passes.objects.get(id=idPass)
-    context = {
-        'balanceNow':wallet.accountBalance,
-        'passBuy':passBuy,
-        'balanceAfter':(wallet.accountBalance-passBuy.price)
-    }
-    return render(request,'show-page-transaction.html',context)
+    try:
+        wallet = Wallet.objects.get(user = request.user)
+        passBuy = Passes.objects.get(id=idPass)
+        context = {
+            'balanceNow':wallet.accountBalance,
+            'passBuy':passBuy,
+            'balanceAfter':(wallet.accountBalance-passBuy.price)
+        }
+        return render(request,'show-page-transaction.html',context)
+    except:
+        raise Http404("Transaction does not exist")
 
 def doTransactionPass(request):
     return ''
