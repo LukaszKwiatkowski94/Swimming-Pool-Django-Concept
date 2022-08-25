6efac1a382489b0a49ee942bd855cfabce8c0602
BuildTools <unconfigured@null.spigotmc.org>
2022-08-25 17:10:18 +0200

    Add superuser to decorators

--- SwimmingPoolDjangoConcept/decorators.py
+++ SwimmingPoolDjangoConcept/decorators.py
@@ -4,7 +4,7 @@ from django.http import Http404
 
 def administrator_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(2), #string == string
+        lambda u: u.is_active and (u.role == str(2) or u.is_superuser), #string == string
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -14,7 +14,7 @@ def administrator_required(function=None, redirect_field_name=REDIRECT_FIELD_NAM
 
 def cashier_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(3),
+        lambda u: u.is_active and (u.role == str(3) or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -24,7 +24,7 @@ def cashier_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, log
 
 def customer_service_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(4),
+        lambda u: u.is_active and (u.role == str(4) or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -34,7 +34,7 @@ def customer_service_required(function=None, redirect_field_name=REDIRECT_FIELD_
 
 def accountant_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(5),
+        lambda u: u.is_active and (u.role == str(5) or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -44,7 +44,7 @@ def accountant_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, 
 
 def press_spokesman_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(6),
+        lambda u: u.is_active and (u.role == str(6) or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
@@ -54,7 +54,7 @@ def press_spokesman_required(function=None, redirect_field_name=REDIRECT_FIELD_N
 
 def management_required(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url='/'):
     decorator = user_passes_test(
-        lambda u: u.is_active and u.role == str(7),
+        lambda u: u.is_active and (u.role == str(7) or u.is_superuser),
         login_url=login_url,
         redirect_field_name=redirect_field_name
     )
