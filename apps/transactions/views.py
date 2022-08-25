e663179562027ce898d2b8bd7d7f70bbef2b0cf2
BuildTools <unconfigured@null.spigotmc.org>
2022-08-25 14:55:09 +0200

    Transaction page style

--- apps/transactions/views.py
+++ apps/transactions/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from apps.user.models import User, Wallet
+from apps.offers.models import Passes
 
 def showPageTransaction(request,idPass):
-    context = {}
+    wallet = Wallet.objects.get(user = request.user)
+    passBuy = Passes.objects.get(id=idPass)
+    context = {
+        'balanceNow':wallet.accountBalance,
+        'passBuy':passBuy,
+        'balanceAfter':(wallet.accountBalance-passBuy.price)
+    }
     return render(request,'show-page-transaction.html',context)
 
 def doTransactionPass(request):
